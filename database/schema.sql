b1ec7929f7809bf9063956157bc3b64be00af00e
James Aguirre <jamesaguirre98@gmail.com>
2023-06-08 20:57:51 +0000

    db schema

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,65 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "products" (
+	"productName" TEXT NOT NULL,
+	"productId" int NOT NULL,
+	"price" real NOT NULL,
+	"categoryId" TEXT NOT NULL,
+	"imageUrl" TEXT NOT NULL
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "Customers" (
+	"customerId" int NOT NULL,
+	"username" TEXT NOT NULL,
+	"password" TEXT NOT NULL,
+	"createdAt" TIMESTAMP NOT NULL
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "shoppingCart" (
+	"productId" int NOT NULL,
+	"customerId" int NOT NULL,
+	"cartId" int NOT NULL
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "orders" (
+	"orderId" int NOT NULL,
+	"cartId" int NOT NULL,
+	"placedAt" TIMESTAMP,
+	"productName" TEXT NOT NULL,
+	"price" real NOT NULL
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "categories" (
+	"categoryId" int NOT NULL,
+	"name" TEXT NOT NULL
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+ALTER TABLE "products" ADD CONSTRAINT "products_fk0" FOREIGN KEY ("categoryId") REFERENCES "categories"("categoryId");
+
+
+ALTER TABLE "shoppingCart" ADD CONSTRAINT "shoppingCart_fk0" FOREIGN KEY ("productId") REFERENCES "products"("productId");
+ALTER TABLE "shoppingCart" ADD CONSTRAINT "shoppingCart_fk1" FOREIGN KEY ("customerId") REFERENCES "Customers"("customerId");
+
+ALTER TABLE "orders" ADD CONSTRAINT "orders_fk0" FOREIGN KEY ("cartId") REFERENCES "shoppingCart"("cartId");
