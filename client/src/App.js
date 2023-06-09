c8ba2002f74733d9115f7a0750b7779cb45a7079
James Aguirre <jamesaguirre98@gmail.com>
2023-06-09 15:57:35 +0000

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import { useEffect, useState } from 'react';
+import NavBar from './Navbar';
+import SignInForm from './SignInForm';
+import RegistrationForm from './RegisterForm';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const [serverData, setServerData] = useState('');
+  /* The current page being displayed:
+   * 'catalog' - A catalog of products
+   * 'register' - The registration page
+   * 'sign-in' - The sign in page
+   * 'cart' - The view cart page
+   */
+  const [page, setPage] = useState('sign-in');
 
   useEffect(() => {
     async function readServerData() {
@@ -18,13 +28,27 @@ function App() {
     readServerData();
   }, []);
 
+  // onclick function for navigating to different pages on the site
+  function handleNavigate(page) {
+    setPage(page);
+    if (page === 'sign-out') {
+      sessionStorage.removeItem('token');
+      setPage('sign-in');
+    }
+  }
+
   return (
-    <div className="App">
+    <>
+      <NavBar onNavigate={handleNavigate} />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>{serverData}</h1>
       </header>
-    </div>
+      {page === 'register' && <RegistrationForm />}
+      {page === 'sign-in' && (
+        <SignInForm onSignIn={() => handleNavigate('catalog')} />
+      )}
+    </>
   );
 }
 
