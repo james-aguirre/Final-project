89ee5f3fc8a665af6abb9698c23ed9686c3a0320
James Aguirre <jamesaguirre98@gmail.com>
2023-06-10 00:26:18 +0000

    login page layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,13 @@
 import { useEffect, useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import NavBar from './Navbar';
 import SignInForm from './SignInForm';
 import RegistrationForm from './RegisterForm';
-import logo from './logo.svg';
+import './layout.css';
 import './App.css';
 
 function App() {
   const [serverData, setServerData] = useState('');
-  /* The current page being displayed:
-   * 'catalog' - A catalog of products
-   * 'register' - The registration page
-   * 'sign-in' - The sign in page
-   * 'cart' - The view cart page
-   */
   const [page, setPage] = useState('sign-in');
 
   useEffect(() => {
@@ -40,10 +35,7 @@ function App() {
   return (
     <>
       <NavBar onNavigate={handleNavigate} />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>{serverData}</h1>
-      </header>
+      <p>{serverData}</p>
       {page === 'register' && <RegistrationForm />}
       {page === 'sign-in' && (
         <SignInForm onSignIn={() => handleNavigate('catalog')} />
--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,41 +1,20 @@
-import './Navbar.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 export default function NavBar({ onNavigate }) {
   return (
-    <header className="header purple-background">
-      <div className="container">
-        <div className="row">
-          <div className="column-full d-flex align-center">
-            <h1 className="white-text">Code Journal</h1>
-            <h3>
-              <button
-                type="button"
-                onClick={() => onNavigate('journal')}
-                className="entries-link white-text">
-                Entries
-              </button>
-              <button
-                type="button"
-                onClick={() => onNavigate('register')}
-                className="entries-link white-text">
-                Register
-              </button>
-              <button
-                type="button"
-                onClick={() => onNavigate('sign-in')}
-                className="entries-link white-text">
-                Sign In
-              </button>
-              <button
-                type="button"
-                onClick={() => onNavigate('sign-out')}
-                className="entries-link white-text">
-                Sign Out
-              </button>
-            </h3>
-          </div>
-        </div>
-      </div>
-    </header>
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">ValTrade</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="catalog">Catalog</Nav.Link>
+            <Nav.Link href="sign-in">Sign in</Nav.Link>
+            <Nav.Link href="register">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
   );
 }
