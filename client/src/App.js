544847793dd6736b1984e82fa13bff8d723470af
James Aguirre <jamesaguirre98@gmail.com>
2023-06-12 22:52:44 +0000

    auth page

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,44 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, createContext } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import NavBar from './Navbar';
+import NavBar from './componenets/Navbar';
+import AppContext from './components/NavBar';
 import RegistrationForm from './RegisterForm';
 import SignInForm from './SignInForm';
 import './layout.css';
 import './App.css';
 
+const tokenKey = 'react-context-jwt';
+
 function App() {
   const [serverData, setServerData] = useState('');
+  const [user, setUser] = useState();
+  const [token, setToken] = useState();
+  const [isAuthorizing, setIsAuthorizing] = useState(true);
   const [page, setPage] = useState('sign-in');
 
   useEffect(() => {
-    async function readServerData() {
-      const resp = await fetch('/api/hello');
-      const data = await resp.json();
-
-      console.log('Data from server:', data);
-
-      setServerData(data.message);
+    const auth = localStorage.getItem(tokenKey);
+    if (auth) {
+      const a = JSON.parse(auth);
+      setUser(a.user);
+      setToken(a.token);
     }
-
-    readServerData();
+    setIsAuthorizing(false);
   }, []);
 
+  if (isAuthorizing) return null;
+  function handleSignIn(auth) {
+    localStorage.setItem(tokenKey, JSON.stringify(auth));
+    setUser(auth.user);
+    setToken(auth.token);
+  }
+  function handleSignOut() {
+    localStorage.removeItem(tokenKey);
+    setUser(undefined);
+    setToken(undefined);
+  }
+
+  const contextValue = { user, token, handleSignIn, handleSignOut };
   // onclick function for navigating to different pages on the site
   function handleNavigate(page) {
     setPage(page);
@@ -34,15 +50,16 @@ function App() {
 
   return (
     <>
-      <NavBar onNavigate={handleNavigate} />
-      <Routes>
-        <Route path="register" element={<RegistrationForm />} />
-        <Route
-          path="sign-in"
-          element={<SignInForm onSignIn={() => handleNavigate('catalog')} />}
-        />
-      </Routes>
-      <p>{serverData}</p>
+      <AppContext.Provider value={contextValue}>
+        <NavBar onNavigate={handleNavigate} />
+        <Routes>
+          <Route path="register" element={<RegistrationForm />} />
+          <Route
+            path="sign-in"
+            element={<SignInForm onSignIn={() => handleNavigate('catalog')} />}
+          />
+        </Routes>
+      </AppContext.Provider>
     </>
   );
 }
