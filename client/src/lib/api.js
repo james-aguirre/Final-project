9b8138a891785eca1b8a864fb0fe9882fcd7a857
James Aguirre <jamesaguirre98@gmail.com>
2023-06-15 16:51:41 +0000

    cart fetch req

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -57,12 +57,8 @@ export async function fetchProduct(productId) {
   return await res.json();
 }
 
-// export async function addToCart(productId) {
-//   const req {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json'
-//     },
-//     body
-//   }
-// }
+export async function fetchCart(cartId) {
+  const res = await fetch('/api/shoppingCart');
+  if (!res.ok) throw new Error(`fetch Error ${res.status}`);
+  return await res.json();
+}
