e7f4456c1f9da66e43c20617a1e27bf342f3b130
James Aguirre <jamesaguirre98@gmail.com>
2023-06-16 22:30:38 +0000

    .

--- client/src/pages/CatalogPage.js
+++ client/src/pages/CatalogPage.js
@@ -2,7 +2,7 @@ import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 import Row from 'react-bootstrap/Row';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { fetchCatalog } from '../lib/api';
 import { Link } from 'react-router-dom';
 import './CatalogPage.css';
@@ -11,7 +11,11 @@ export default function Catalog({ product }) {
   const [products, setProducts] = useState();
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState();
+  const [filter, setFilter] = useState('');
 
+  // const searchFilter = useCallback(() => {
+  //   return products.filter((p) => p.name.includes(filter));
+  // }, [products, filter]);
   useEffect(() => {
     async function loadCatalog() {
       try {
@@ -29,6 +33,17 @@ export default function Catalog({ product }) {
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error Loading Catalog: {error.message}</div>;
 
+  function Filter({ filter, onChange }) {
+    return (
+      <input
+        type="text"
+        value={filter}
+        onChange={(e) => onChange(e.target.value)}
+        placeholder="Search me!"
+      />
+    );
+  }
+
   return (
     <Container fluid className="catalog-container">
       <div className="banner-container">
@@ -38,7 +53,27 @@ export default function Catalog({ product }) {
           alt="phoenix jett valorant banner"
         />
       </div>
-      <h1 className="catalog-header">Skins Catalog</h1>
+      <Row>
+        <form>
+          <div className="row">
+            <div className="col-third">
+              <select aria-label="small" className="mb-3">
+                <option>Filter by weapon</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+            </div>
+            <div className="col-third">
+              <h1 className="catalog-header">Skins Catalog</h1>
+            </div>
+            <div className="col-third">
+              <Filter value={filter} onChange={setFilter} />
+            </div>
+          </div>
+        </form>
+        {/* <h1 className="catalog-header">Skins Catalog</h1> */}
+      </Row>
       <Row xs="auto">
         {products?.map((product) => (
           <Col xs={6} md={4} className="card-wrapper" key={product.productId}>
