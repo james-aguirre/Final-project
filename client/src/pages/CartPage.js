e8c2c56c9504ce8dc1f8ca6e2b917904055b1fd7
James Aguirre <jamesaguirre98@gmail.com>
2023-06-19 17:03:36 +0000

    layout for cart items table

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -4,13 +4,14 @@ import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
+// import { Link } from 'react-router-dom';
 
 export default function Cart() {
   return (
     <Container className="container-cart" fluid>
       <Row className="header justify-space-between row-cart">
         <Col sm={8}>
-          <h2>Shopping Cart</h2>
+          <h2>Your Cart</h2>
         </Col>
         <Col>
           <Button className="continue-shopping-btn" sm={4}>
@@ -18,18 +19,22 @@ export default function Cart() {
           </Button>
         </Col>
       </Row>
+      <Row className="cart-items-header product-details">
+        <Col xs={6}>Cart Items</Col>
+        <Col>Price</Col>
+        <Col>Quantity</Col>
+        <Col>Subtotal</Col>
+      </Row>
       <Row className="product-details">
-        <Col smaller>
-          <Image className="img preview" thumbnail />
-        </Col>
         <Col xs={6} className="justify-space-between">
+          <Image className="img preview" thumbnail />
           <h1>Product Name</h1>
-          <div>$25</div>
         </Col>
+        <Col className="price">$25</Col>
         <Col>
           <div>1</div>
-          <Button>Delete</Button>
         </Col>
+        <Col className="margin-right">50$</Col>
       </Row>
     </Container>
   );
