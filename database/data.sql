dd1220c29a3edcec455459608bff1c2329ad3d56
James Aguirre <jamesaguirre98@gmail.com>
2023-06-19 04:31:49 +0000

    added some new items to shop

--- database/data.sql
+++ database/data.sql
@@ -1,40 +1,50 @@
--- Use SQL insert statements to add any
--- starting/dummy data to your database tables
-
--- EXAMPLE:
-
---  insert into "todos"
---    ("task", "isCompleted")
---    values
---      ('Learn to code', false),
---      ('Build projects', false),
---      ('Get a job', false);
 
 
 insert into "products" ( "productName", "price", "category", "imageUrl", "description")
 values
-( 'blastX Knife', 9.99, 'melee', 'https://cx.valorbuff.com/blob/BRcfB9CUanCZK+h7l1pOKnsblVHqBtxJKVvShXlN56sbl+c0hVCZ5axNhX-Gva0E?w=900', 'base level variant'),
-( 'Glitchpop Knife', 9.99, 'melee', 'https://cx.valorbuff.com/blob/BRcfB9CUanC2l1JOKN-rlnbqitDXBtxJ5+USh+pVK2sr51DXlVp7l1-YitpGva0E?w=900', 'base level variant'),
-( 'Champions Karambit', 19.99, 'melee', 'https://cx.valorbuff.com/blob/BRcfB9CUanC0i+h7iNc2h6sOlX3nBtxXK1KSht9bK2bqlVhOKacZl+L3KXpGva0E?w=900', 'base level variant'),
+( 'blastX Knife', 14.99, 'melee', 'https://cx.valorbuff.com/blob/BRcfB9CUanCZK+h7l1pOKnsblVHqBtxJKVvShXlN56sbl+c0hVCZ5axNhX-Gva0E?w=900', 'base level variant'),
+( 'Glitchpop Knife', 14.99, 'melee', 'https://cx.valorbuff.com/blob/BRcfB9CUanC2l1JOKN-rlnbqitDXBtxJ5+USh+pVK2sr51DXlVp7l1-YitpGva0E?w=900', 'base level variant'),
+( 'G.U.N.Knife', 19.99, 'melee', 'https://cx.valorbuff.com/blob/BRcfB9CUan3XitDXhVD3insshNcZBtx3lXKS5tc7h2sOhNlnlV3qlVWqlNpGva0E?w=900', 'base level variant'),
 ( 'Ruination Knife', 19.99, 'melee', 'https://cx.valorbuff.com/blob/BRcfB9CUanCYiac7lXDNKIs2Kt3nBtcYi+hS5aini6sbKahOiacVhVU0KNUGva0E?w=900', 'level 6 variant'),
 ( 'Winterwunderland Knife', 9.99, 'melee', 'https://cx.valorbuff.com/blob/BRcfB9CUanCsKXWn5tDqinbXlViJBtxNK19S5+lJh6bNhVKshtcrlrKYlr-Gva0E?w=900', 'level 4 variant'),
-( 'Recon Knife', 9.99, 'melee','https://cx.valorbuff.com/blob/BRcfB9CUanC0lXKOKNUY56bq519OBtcVhVKShtW3lIssht3Jl1Jri+-0lVKGva0E?w=900', 'level 5 variant'),
-( 'Kingdom Knife', 9.99, 'melee','https://cx.valorbuff.com/blob/BRcfB9CUanC0lXKOKNUY56bq519OBtcVhVKShtW3lIssht3Jl1Jri+-0lVKGva0E?w=900', 'base level variant'),
-( 'Reaver Knife', 9.99, 'melee','https://cx.valorbuff.com/blob/BRcfB9CUanCVlXJ7Kap2lns2hriqBtxXht9S51-0h2b3KXK0lt92KX-b5tUGva0E?w=900', 'base level variant'),
-( 'Snowfall Wand', 14.99, 'melee','https://cx.valorbuff.com/blob/BRcfB9CUanCO5ahZhtqNlIssKtKOBtcshNUSh+x356bJhNc2K1v0KVhriaKGva0E?w=900', 'base level variant'),
+( 'Recon Knife', 14.99, 'melee','https://cx.valorbuff.com/blob/BRcfB9CUanC0lXKOKNUY56bq519OBtcVhVKShtW3lIssht3Jl1Jri+-0lVKGva0E?w=900', 'level 5 variant'),
+( 'Kingdom Knife', 9.99, 'melee','https://cx.valorbuff.com/blob/BRcfB9CUanCsl1csitUVKns0K1HXBtxN51US5tiNK2b3iah0lahriXQX5thGva0E?w=900', 'base level variant'),
+( 'Forsaken Knife', 19.99, 'melee','https://cx.valorbuff.com/blob/BRcfB9CUanCb5t-ZlXhYKnbqKr-sBtcrl1KShtcYinbnlt-ZhXC0lrLJltpGva0E?w=900', 'base level variant'),
+( 'Snowfall Wand', 94.99, 'melee','https://cx.valorbuff.com/blob/BRcfB9CUanCO5ahZhtqNlIssKtKOBtcshNUSh+x356bJhNc2K1v0KVhriaKGva0E?w=900', 'Limited edition'),
 ( 'Ego Knife', 14.99, 'melee','https://cx.valorbuff.com/blob/BRcfB9CUanCs5aLXKahOl2s0i1Q3BtcrKVCShX-bK2s2hXh2lX3XKtDXhNhGva0E?w=900', 'level 4 variant'),
 ( 'Champions Vandal', 19.99, 'rifle', 'https://cx.valorbuff.com/blob/BRcfB9CUanC2i1Q3Ka-7insr51pZBtcriXJShNQNKnsVlaK2hrcZiXHqi1hGva0E?w=900', 'base level variant'),
 ( 'Prime Vandal', 29.99, 'rifle', 'https://cx.valorbuff.com/blob/BRcfB9CUanC2KrL3i1C7h6s75tDJBtxXitJShX-2lIsriXLNi1-rl+hr51CGva0E?w=900', 'level 7 variant'),
 ( 'RGX 11z Pro Vandal', 29.99, 'rifle', 'https://cx.valorbuff.com/blob/BRcfB9CUan3nlri3KNHnlIbJKVhbBtc0hX9S5txXi6s2hXQ3lriN5aUbi+pGva0E?w=900', 'base level variant'),
 ( 'Reaver Vandal', 19.99, 'rifle', 'https://cx.valorbuff.com/blob/BRcfB9CUanCOhVp2laUZh2bNKtLXBtxnKXvS5apZ56bJKNpYlXq35tLNlaKGva0E?w=900', 'level 7 variant'),
 ( 'Ego Vandal', 19.99, 'rifle', 'https://cx.valorbuff.com/blob/BRcfB9CUan3JitCrhX-0h6bX5aLnBtcZit9ShX-Vl2bqiaQX5+UrKV335aKGva0E?w=900', 'base level variant'),
+( 'Ion Phantom', 24.99, 'rifle', 'https://cx.valorbuff.com/blob/BRcfB9CUanCYKVHNhXlqhnsV5tiqBtxnht9S51Crh2sYlNHnlVcZi1iJKVKGva0E?w=900', 'base level variant'),
+( 'Prime 2.0 Phantom', 19.99, 'rifle', 'https://cx.valorbuff.com/blob/BRcfB9CUan33i+UOiXW3i6s2K1q3BtcrlX9ShXl35Ib3hXHXKXD3lapViXpGva0E?w=900', 'base level variant'),
+( 'Spectrum Phantom', 29.99, 'rifle', 'https://cx.valorbuff.com/blob/BRcfB9CUanCbKVUr51DXi6b3iXlnBtcbKr-ShtD3h2s0iXDJitxqlthVlrpGva0E?w=900', 'level 7 variant'),
+( 'Spectrum Guardian', 29.99, 'rifle', 'https://cx.valorbuff.com/blob/BRcfB9CUanCb5tJZKNcYK6ssKt90BtcYitcS5aiqhns7KrU0hthZl+U2ltvGva0E?w=900', 'level 7 variant'),
 ( 'ElderFlame Operator', 29.99, 'sniper', 'https://cx.valorbuff.com/blob/BRcfB9CUan3NKtK7KV-Ylns0KrpZBtxJitvShtlNK2s7itHJlXhsi1U0hV-Gva0E?w=900', 'level 5 variant'),
-( 'Reaver Operator', 9.99, 'sniper', 'https://cx.valorbuff.com/blob/BRcfB9CUanCVlVCr5tHXh2bNi1-2BtcViN-S5tHqhnbJK1lnlXKsl+lnKXhGva0E?w=900', 'base level variant'),
-( 'Reaver Operator', 14.99, 'sniper', 'https://cx.valorbuff.com/blob/BRcfB9CUanCbh+U2hrKZlIssKtQqBtc7l+cS5tJYK6sblVQXi1hOl+K7lVJGva0E?w=900', 'level 7 variant'),
+( 'Reaver Operator', 29.99, 'sniper', 'https://cx.valorbuff.com/blob/BRcfB9CUanCVlVCr5tHXh2bNi1-2BtcViN-S5tHqhnbJK1lnlXKsl+lnKXhGva0E?w=900', 'base level variant'),
+( 'Reaver Operator', 24.99, 'sniper', 'https://cx.valorbuff.com/blob/BRcfB9CUanCbh+U2hrKZlIssKtQqBtc7l+cS5tJYK6sblVQXi1hOl+K7lVJGva0E?w=900', 'level 7 variant'),
 ( 'Spline Operator', 19.99, 'sniper', 'https://cx.valorbuff.com/blob/BRcfB9CUan3NiNcrhN-7ins05+c0BtcYl+hShtcOhnbN5+p2l13Xi+iJKNUGva0E?w=900', 'base level variant'),
 ( 'Luxe Operator', 19.99, 'sniper', 'https://cx.valorbuff.com/blob/BRcfB9CUan3nKVJ0htJsK6b3i1iJBtcbiXcShXLX56bNlVCYitHni1CbKN-Gva0E?w=900', 'base level variant'),
+( 'Avalanche Marshall', 19.99, 'sniper', 'https://cx.valorbuff.com/blob/BRcfB9CUanC0KXWni+K2h6sYi+UZBtc7KXJShtxqlIbXKrKOKaUOiX9VhNhGva0E?w=900', 'limited edition'),
+( 'Couture Marshall', 19.99, 'sniper', 'https://cx.valorbuff.com/blob/BRcfB9CUanCritQnhVc2l6b351vVBtc7lVJShNxnhnsrK+h2K1W3l1UYlNhGva0E?w=900', 'limited edition'),
+( 'Sovereign Marshall', 19.99, 'sniper', 'https://cx.valorbuff.com/blob/BRcfB9CUanCOKrK0ith056bni+hsBtc7K+KS51p0lIb3KtJVK1Jbh+c25+cGva0E?w=900', 'limited edition'),
+
+
+
+
+
 ( 'Singularity Sheriff', 19.99, 'sidearm', 'https://cx.valorbuff.com/blob/BRcfB9CUan3XK+K2lax3iIs7lthOBtxNKtpS51CsinssK1Cb5thOK1cZlNhGva0E?w=900', 'level 7 variant'),
 ( 'Reaver Sheriff', 19.99, 'sidearm', 'https://cx.valorbuff.com/blob/BRcfB9CUanC0KXJVltUVi6ssK1K2BtcZKXcS51CrKnbXhrK0lapVhr-2K1hGva0E?w=900', 'level 6 variant'),
 ( 'Peacekeeper Sheriff', 14.99, 'sidearm', 'https://cx.valorbuff.com/blob/BRcfB9CUanCbltDN5tv256sOl192BtcYlVcSh+Ksinsrit3nlVC051UYitcGva0E?w=900', 'base level variant'),
 ( 'Ronin Sheriff', 14.99, 'sidearm', 'https://cx.valorbuff.com/blob/BRcfB9CUanCriXUsKaK2KnbJhVcOBtxnlN-Sh+Lq56sZh+LNKahYKNpsiXUGva0E?w=900', 'base level variant'),
-( 'Aristocrat Sheriff', 24.99, 'sidearm', 'https://cx.valorbuff.com/blob/BRcfB9CUanCsla-2lVJ7h6s2h+c2Btxn5thShthYl6sV51U0ialNlNQNKVUGva0E?w=900', 'base level variant');
+( 'Aristocrat Sheriff', 19.99, 'sidearm', 'https://cx.valorbuff.com/blob/BRcfB9CUanCsla-2lVJ7h6s2h+c2Btxn5thShthYl6sV51U0ialNlNQNKVUGva0E?w=900', 'base level variant'),
+( 'Dot Exe Judge', 14.99, 'shotgun', 'https://cx.valorbuff.com/blob/BRcfB9CUanC7KXcViaKVKnsYlXJVBtxJltJShNcZh6bXi+hsKahbhNxN51vGva0E?w=900', 'base level variant'),
+( 'ElderFlame Judge', 19.99, 'shotgun', 'https://cx.valorbuff.com/blob/BRcfB9CUanCb5tDXi+UbinsZKViqBtxnhVvShXL3lnsslXiXh+hri1LNl1CGva0E?w=900', 'base level variant'),
+( 'Glitchpop Judge', 19.99, 'shotgun', 'https://cx.valorbuff.com/blob/BRcfB9CUan3351DXi+-0l2s7l+KbBtcbh+cS5tlqlnbJitxnlrHqiaxn5+pGva0E?w=900', 'base level variant'),
+( 'Oni Bucky', 24.99, 'shotgun', 'https://cx.valorbuff.com/blob/BRcfB9CUan3JlNK2hrc0hnsY5+UsBtxJKtCS5aH3KIb3K1Ksi+KZltQNi1KGva0E?w=900', 'base level variant'),
+( 'Prime 2.0 Bucky', 19.99, 'shotgun', 'https://cx.valorbuff.com/blob/BRcfB9CUanCsitDJ5aH3KIbJhVWJBtcYhthShtvrl2b3iNh0KXJVKr-751-Gva0E?w=900', 'base level variant'),
+( 'Kingdom Bucky', 19.99, 'shotgun', 'https://cx.valorbuff.com/blob/BRcfB9CUan3ql1-05t9bKIsbhV-YBtc2lVcShNpYh2sZiNcVltHXiaU2KNKGva0E?w=900', 'Limited edition'),
+( 'Spitfire Frenzy', 24.99, 'sidearm', 'https://cx.valorbuff.com/blob/BRcfB9CUanCshV9sltlNlnbq5ah2BtcYK1KShtc7lIsZKViNhVHJ51L3iXvGva0E?w=900', 'Limited edition'),
+( 'RGX 11 Pro Frenzy', 24.99, 'sidearm', 'https://cx.valorbuff.com/blob/BRcfB9CUanCsi+QJKXKViIbnKNp2BtcYlNUSh+-bhnbNK1HniahYKNQqKVvGva0E?w=900', 'Limited edition'),
+( 'Elderflame Frenzy', 29.99, 'sidearm', 'https://cx.valorbuff.com/blob/BRcfB9CUanCVi1QXKtvrKIbq5ap0BtcOhrKS5tQqlnsYl+xqh+cVKaUsi1vGva0E?w=900', 'Level 7 variant')
