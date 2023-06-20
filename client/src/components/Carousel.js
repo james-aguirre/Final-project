a005cbdc8c0da7cec565030d0ccf94e223a57be4
James Aguirre <jamesaguirre98@gmail.com>
2023-06-20 21:36:54 +0000

    css for carousel component

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
-import './CarouselComponent.css';
+import './Carousel.css';
 
 export default function Carousel({ images }) {
   const [activeIndex, setIndex] = useState(0);
